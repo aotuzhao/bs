7379df21762d668e5eff069cfd6397baf9f9f665
aotuzhao <zxzhao1994@126.com>
2019-04-18 14:07:35 +0800

    add high availability eureka server

--- bbs-eureka-slave/src/main/java/com/magpie/group/bbseurekaslave/EurekaSlaveApplication.java
+++ bbs-eureka-slave/src/main/java/com/magpie/group/bbseurekaslave/EurekaSlaveApplication.java
@@ -1,4 +1,4 @@
-package com.magpie.bbs.eureka;
+package com.magpie.group.bbseurekaslave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +12,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaSlaveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaSlaveApplication.class, args);
     }
+
 }
